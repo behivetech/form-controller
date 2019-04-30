1dfaaf9689100bfa9dfc519b9af81c85c8fa8421
Bruce Smith <jbsmith@smithcasa.net>
2019-04-30 13:21:01 -0600

    Initial commit with useFormController and the test file.

---
+++ src/useFormController.test.js
@@ -0,0 +1,43 @@
+// useFormController.test.js
+// import React from 'react';
+import {render} from 'react-testing-library';
+import useFormController from './useFormController';
+
+describe('useFormController', () => {
+    const mockHandleSubmit = jest.fn();
+
+    function renderForm(useFormControllerArgs) {
+        const {
+            fieldErrors,
+            formValues,
+            formProps,
+            fieldProps,
+            submitButtonProps,
+        } = useFormController({
+            fieldProps: {
+                mockCheckbox: {
+                    checked: true,
+                    value: 'mock checkbox value',
+                },
+                mockNoSubmitTextField: {
+                    noSubmit: true,
+                    value: 'mock no submit text value'
+                }
+            },
+            onSubmit: mockHandleSubmit,
+        });
+
+        return (
+            <form name="mockFormName" {...formProps} >
+                <input name="mockNoSubmitTextField" {...fieldProps} />
+                <input type="checkbox" name="mockCheckbox" {...fieldProps} />
+            </form>
+        );
+    }
+
+    it('should validate fields correctly', () => {
+        const {container, debug} = render(renderForm());
+
+        debug();
+    });
+});
---
+++ src/useFormController.tsx
@@ -0,0 +1,266 @@
+import * as React from 'react';
+import * as lodash from 'lodash';
+
+const {useState} = React;
+const {
+    forEach,
+    get,
+    isArray,
+    isEmpty,
+    isFunction,
+    mapValues,
+    omit,
+    transform,
+} = lodash;
+
+interface useFormControllerArgs {
+   fieldProps: {
+       [key: string]: {
+           checked?: Boolean
+           noSubmit?: Boolean
+           value?: string | number | [string | number]
+           validation?: () => Boolean | [() => Boolean]
+       }
+   };
+   onSubmit: Function;
+}
+
+interface useFormControllerResponse {
+    fieldErrors: {
+        [key: string]: string
+    };
+    formValues: {
+        [key: string]: string
+    };
+    formProps: {
+        [key: string]: {
+            onSubmit: Function
+        }
+    };
+    fieldProps: {
+        checked?: Boolean;
+        disabled: Boolean | undefined;
+        ref: ()=> {
+            inputElement: {value: string}
+        };
+        value: string | number | [string | number];
+    }
+    submitButtonProps: {
+        disabled: Boolean | undefined;
+    }
+}
+
+interface formValues {
+    [key: string]: string | number | [string | number]
+}
+
+interface fieldRefs {
+    [key: string]: {
+        inputElement: {
+            value: string
+        }
+    }
+}
+
+interface fieldErrors {
+    [key: string]: string
+}
+
+export default function useFormController<useFormControllerResponse>({
+    fieldProps,
+    onSubmit,
+}: useFormControllerArgs) {
+    const [formValues, setFormValues] = useState<formValues>(mapValues(fieldProps, 'value') || {});
+    const [fieldRefs, setFieldRefs] = useState<fieldRefs>({});
+    const [fieldErrors, setFieldErrors] = useState<fieldErrors>({});
+    const [formIsSubmitting, setFormIsSubmitting] = useState<Boolean>(false);
+    const [initialSubmit, setInitialSubmit] = useState<Boolean>(false);
+
+    function setFormValuesState({
+            name,
+            checked,
+            type,
+            value,
+        }:
+        {
+            name: string,
+            checked: Boolean | undefined,
+            type: string
+            value: string | number | [string | number]
+        }
+    ) {
+        const fieldValue = (checked || type !== 'checkbox') ? value : "";
+
+        setFormValues({
+            ...formValues,
+            [name]: fieldValue,
+        });
+    }
+
+    function setFieldRefsState(name: string, inputRef: any) {
+        setFieldRefs({
+            ...fieldRefs,
+            [name]: inputRef,
+        });
+    }
+
+    function initField(inputRef: Function) {
+        // Some componet libraries return inputElement as the actual ref
+        // May need to add more if other libraries are used
+        const inputElement = get(inputRef, 'inputElement') || inputRef;
+
+        if (inputElement) {
+            const {checked, name, type, value} = inputElement;
+
+            if (!name) {
+                // eslint-disable-next-line
+                console.error(
+                    'useFormSubmission: A name attribute must be specified for this element',
+                    {inputElement}
+                );
+            } else if (!fieldRefs[name]) {
+                setFieldRefsState(name, inputElement);
+                !fieldProps[name].noSubmit && setFormValuesState({
+                    name,
+                    checked,
+                    type,
+                    value,
+                });
+            }
+        } else {
+            // eslint-disable-next-line
+            console.error(
+                'useFormSubmission: Could not set a ref for this form field',
+                {inputRef}
+            );
+        }
+    }
+
+    function validateField(name: string, value: string | number | [string | number]) {
+        const validateFunctions = fieldProps[name].validation;
+        let fieldError: string;
+
+        if (validateFunctions) {
+            const fieldValidation = (isArray(validateFunctions)) ? validateFunctions : [validateFunctions];
+
+            // Using the lodash transform to run until it finds the first error
+            // This way it only shows one validation error at a time until
+            // they are all gone if there are multiples.
+            transform(fieldValidation, (fieldErrors, validator) => {
+                if (!isFunction(validator)) {
+                    // eslint-disable-next-line
+                    console.error('useFormController: Field validators must be functions', {validator})
+                } else {
+                    fieldError = validator(value);
+                }
+
+                return !fieldError;
+            });
+
+            if (fieldError) {
+                setFieldErrors({...fieldErrors, [name]: fieldError});
+            } else {
+                setFieldErrors(omit(fieldErrors, name));
+            }
+        }
+
+        return fieldError;
+    }
+
+    function getFieldsToValidate(): string[] {
+        let fieldsToValidate:string[] = [];
+
+        forEach(fieldProps, (fieldProp, key) => {
+            if (fieldProp.validation) {
+                fieldsToValidate.push(key)
+            }
+        })
+
+        return fieldsToValidate;
+    }
+
+    function validateAllFields() {
+        let fieldsHaveError;
+
+        transform(getFieldsToValidate(), (fieldErrors: Object, fieldName: string) => {
+            const fieldError = validateField(
+                fieldName,
+                fieldRefs[fieldName].inputElement.value,
+            );
+
+            fieldsHaveError = !!fieldError
+            return !fieldError;
+        });
+
+        return fieldsHaveError;
+    }
+
+    function handleFieldChange(event: React.FormEvent<HTMLInputElement>) {
+        event.preventDefault();
+
+        const {
+            checked,
+            name,
+            type,
+            value
+        } = event.currentTarget;
+
+        setFormValuesState({
+            name,
+            checked,
+            type,
+            value,
+        });
+        validateField(name, value);
+    }
+
+    function handleFormSubmit(event: React.FormEvent<HTMLInputElement>) {
+        event.preventDefault();
+
+        let fieldsHaveError;
+
+        setFormIsSubmitting(true);
+
+        if (!initialSubmit) {
+            // Validating all fields for autofill values if there hasn't been an initial submit
+            fieldsHaveError = validateAllFields();
+        }
+
+        if (!fieldsHaveError) {
+            executeForm();
+        }
+
+        setInitialSubmit(true);
+    }
+
+    async function executeForm() {
+        await (onSubmit instanceof Promise)
+            ? onSubmit()
+            : Promise.resolve(onSubmit());
+
+        setFormIsSubmitting(false);
+    }
+
+    const submitButtonProps = {
+        disabled: formIsSubmitting || (!initialSubmit && !isEmpty(fieldErrors)),
+    }
+
+    const formFieldProps = {
+        disabled: formIsSubmitting,
+        onChange: handleFieldChange,
+        ref: initField,
+    }
+
+    const formProps = {
+        onChange: handleFormSubmit
+    };
+
+    return {
+        fieldErrors,
+        formValues,
+        formProps,
+        fieldProps: formFieldProps,
+        submitButtonProps,
+    };
+
+}
